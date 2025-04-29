19640b3037620ed9d2444f6689b79d015d5c84ea
timjonaswechler <code.timjonaswechler@icloud.com>
2025-04-29 17:30:14 +0200

    Remove local forge_ui – jetzt als Submodul

--- crates/forge_ui/src/ui_elements/button/events.rs
+++
@@ -1,8 +0,0 @@
-use bevy::prelude::*;
-
-/// Event sent when a non-disabled button is clicked (Pressed state).
-#[derive(Event, Debug, Clone)]
-pub struct ButtonClickedEvent {
-    /// The entity ID of the button that was clicked.
-    pub button_entity: Entity,
-}
