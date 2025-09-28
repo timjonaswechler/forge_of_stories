d58713f154de6de5583450cd677f8624bdee2834
Tim-Jonas Wechler <tim-jonaswechler@macbookpro.speedport.ip>
2025-09-28 21:31:54 +0200

    Change directory paths to static references

--- crates/app/src/app.rs
+++ crates/app/src/app.rs
@@ -9,10 +9,9 @@ pub type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub struct AppBase {
     pub app_id: &'static str,
     pub version: &'static str,
-    pub config_dir: PathBuf,
-    pub data_dir: PathBuf,
-    pub logs_dir: PathBuf,
-    /// Guard to keep the non-blocking tracing worker alive (flushes log lines).
+    pub config_dir: &'static PathBuf,
+    pub data_dir: &'static PathBuf,
+    pub logs_dir: &'static PathBuf,
     pub log_guard: tracing_appender::non_blocking::WorkerGuard,
 }
 
@@ -38,9 +37,9 @@ pub trait Application: Sized + 'static {
 pub fn init<A: Application>(version: &'static str) -> Result<AppBase, A::Error> {
     let app_id = A::APP_ID;
 
-    let config_dir = paths::config_dir().join(app_id);
-    let data_dir = paths::data_dir().join(app_id);
-    let logs_dir = paths::logs_dir().join(app_id);
+    let config_dir = paths::config_dir();
+    let data_dir = paths::data_dir();
+    let logs_dir = paths::logs_dir();
 
     let current_local: DateTime<Local> = Local::now();
     let custom_format = current_local.format("%Y-%m-%dT%H:%M:%S").to_string();
@@ -49,7 +48,7 @@ pub fn init<A: Application>(version: &'static str) -> Result<AppBase, A::Error> 
     fs::create_dir_all(&config_dir).ok();
     fs::create_dir_all(&data_dir).ok();
     fs::create_dir_all(&logs_dir).ok();
-    tracing::error!("{:?}", logs_dir);
+
     let file_appender = tracing_appender::rolling::never(
         &logs_dir,
         paths::log_file(app_id, custom_format.as_str()),
