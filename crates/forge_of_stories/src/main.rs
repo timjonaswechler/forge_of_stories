77d9cdc69d97bda36ea59b1672c84249ace483b6
Tim-Jonas Wechler <code.timjonaswechler@icloud.com>
2025-11-08 20:03:35 +0100

    cleanup

--- crates/forge_of_stories/src/main.rs
+++ crates/forge_of_stories/src/main.rs
@@ -12,7 +12,7 @@ use app::AppBuilder;
 use bevy::{log::LogPlugin, prelude::*};
 use bevy_enhanced_input::prelude::*;
 use game_server::settings::Network;
-use keymap::KeymapPlugin;
+
 use settings::{AppSettingsExt, SettingsStore};
 
 /// Game state tracking where we are in the application flow.
@@ -56,7 +56,6 @@ fn main() {
 
             // Add EnhancedInputPlugin BEFORE KeymapInputPlugin
             app.add_plugins((
-                KeymapPlugin::with_config_path(ctx.path_context().keybinding_file()),
                 EnhancedInputPlugin, // TODO: in port in keymap plugin
                 NetworkingPlugin,
                 InputPlugin,
